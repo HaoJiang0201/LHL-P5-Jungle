289031e97d59f1473fd3f3ce667c2298512602e8
HaoJiang0201 <haojiang0201@gmail.com>
2019-04-18 20:33:07 +0000

    Flash Message Comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,23 +3,7 @@ class UsersController < ApplicationController
     def new
     end
 
-   
-
     def create
-        # user = User.new(
-        #     name: params[:name],
-        #     email: params[:email],
-        #     password: params[:password],
-        #     password_confirmation: params[:password_confirmation]
-        # )
-        # if user.save
-        #     session[:user_id] = user.id
-        #     flash[:success] = "Successfully Created User!"
-        #     redirect_to '/products'
-        # else
-        #     flash[:warning] = "Invalid Email or Password!"
-        #     redirect_to '/signup'
-        # end
         
         user = User.new(user_params)
         if user.save
