f412ceec3b9f0948adcc15cac0d55c32ae07c3fe
HaoJiang0201 <haojiang0201@gmail.com>
2019-04-19 20:07:08 +0000

    Rating function completed and show page style adjusted.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,6 +6,16 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find params[:id]
+    @reviews = @product.reviews.order(created_at: :desc)
+    @enhanced_reviews = []
+    @reviews.each do |review|
+      user = User.find(review[:user_id])
+      enhanced_review = {}
+      enhanced_review[:review] = review
+      enhanced_review[:user] = user[:name]
+      @enhanced_reviews.push(enhanced_review)
+    end
+    puts ">>> enhanced_reviews: #{@enhanced_reviews[0]}"
   end
 
 end
