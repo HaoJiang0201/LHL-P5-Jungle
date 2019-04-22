01ade30dac7acdd8ca2ee6ede6ed646877f41482
HaoJiang0201 <haojiang0201@gmail.com>
2019-04-22 16:40:07 +0000

    reviews code cleanup

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,6 @@
 class ReviewsController < ApplicationController
 
     def create
-        
         @review = Review.new(review_params)
         @review.user_id = current_user.id
         @review.product_id = params[:product_id]
